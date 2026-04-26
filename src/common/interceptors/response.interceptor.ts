dbbc7eb078e2ace560ee442e346d416cdd9a1ec9
eunwoo-levi <eunwoo1341@gmail.com>
2026-04-26 16:01:31 +0900

    refactor: ResponseInterceptor 커스텀 응답 메시지 지원

--- src/common/interceptors/response.interceptor.ts
+++ src/common/interceptors/response.interceptor.ts
@@ -4,20 +4,22 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { RESPONSE_MESSAGE_KEY } from '../decorators/response-message.decorator';
 
 @Injectable()
 export class ResponseInterceptor<T> implements NestInterceptor<T, unknown> {
-  intercept(
-    _context: ExecutionContext,
-    next: CallHandler<T>,
-  ): Observable<unknown> {
-    return next.handle().pipe(
-      map((data) => ({
-        message: '성공',
-        data,
-      })),
-    );
+  constructor(private readonly reflector: Reflector) {}
+
+  intercept(context: ExecutionContext, next: CallHandler<T>): Observable<unknown> {
+    const message =
+      this.reflector.getAllAndOverride<string>(RESPONSE_MESSAGE_KEY, [
+        context.getHandler(),
+        context.getClass(),
+      ]) ?? '성공';
+
+    return next.handle().pipe(map((data) => ({ message, data })));
   }
 }
